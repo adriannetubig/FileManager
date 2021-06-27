78ddcf70e98dcd11d0bf3b7cbd7cc2fee6cce68e
Adrianne Claude Tubig <art0901@carenethealthcare.com>
2021-06-28 01:00:13 +0800

    Make it work

---
+++ FileManagerDaemonConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace FileManagerDaemonConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ReadFilesandSubFolders("\\\\MSI\\Shared");
+        }
+
+        static void ReadAllFilesInFolder(string folder)
+        {
+            foreach (string file in Directory.EnumerateFiles(folder))
+            {
+                Console.WriteLine(file);
+            }
+        }
+
+        static void ReadFilesandSubFolders(string folder)
+        {
+            ReadAllFilesInFolder(folder);
+            foreach (string subFolder in Directory.EnumerateDirectories(folder))
+            {
+                ReadFilesandSubFolders(subFolder);
+            }
+        }
+    }
+}
